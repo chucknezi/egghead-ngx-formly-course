6ff08a0acf14cd73d533316e2217e9f93ac73cf1
ChuckNezi <jnezia1@gmail.com>
2020-04-28 19:15:18 -0400

    built first form using formly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,30 @@
 import { Component } from '@angular/core';
+import { FormlyFieldConfig } from '@ngx-formly/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {}
+export class AppComponent {
+ form = new FormGroup({});
+ model = {
+   firstName: 'Juri',
+   age: 34
+ };
+ fields: FormlyFieldConfig[] = [
+   {
+      key: 'firstname',
+      type: 'input',
+      templateOptions: {
+        label: 'Firstname'
+      }
+
+   }
+
+ ];
+
+ onSubmmit({valid, value}){
+   console.log(value);
+ }
+}
